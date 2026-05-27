25af6ee858f14244a6dd1ecd72919616f7267bee
ora-nova <shyakauwaseoranova@gmail.com>
2026-05-27 10:43:17 +0200

    question3

---
+++ question3.js
@@ -0,0 +1,41 @@
+function flattenAndUnique(nestedArray) {
+    let result = [];
+
+    for (const item of nestedArray) {
+
+        if (Array.isArray(item)) {
+
+            for (const value of item) {
+
+                if (Array.isArray(value)) {
+
+                    for (const num of value) {
+                        if (!result.includes(num)) {
+                            result.push(num);
+                        }
+                    }
+
+                } else {
+
+                    if (!result.includes(value)) {
+                        result.push(value);
+                    }
+
+                }
+            }
+
+        } else {
+
+            if (!result.includes(item)) {
+                result.push(item);
+            }
+
+        }
+    }
+
+    return result;
+}
+
+const nestedArray = [1, [2, 3], 4, [2, [5, 1]], 3];
+const result = flattenAndUnique(nestedArray);
+console.log(result); // Output: [1, 2, 3, 4, 5]
